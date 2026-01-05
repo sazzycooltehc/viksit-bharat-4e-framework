393ae833f99c2206c6ad7aea518f382b1d31af1c
sazzycooltehc <aravindsas999@gmail.com>
2026-01-05 23:53:08 +0530

    IM: tsconfig

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideZonelessChangeDetection } from '@angular/core';
-import { AppComponent } from './app.component';
+import { AppComponent } from './src/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
