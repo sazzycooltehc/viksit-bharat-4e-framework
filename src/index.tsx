f01b216a78a938461de9e3e333e2cdfe7176835c
sazzycooltehc <aravindsas999@gmail.com>
2026-01-05 23:49:55 +0530

    modified:   src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideZonelessChangeDetection } from '@angular/core';
-import { AppComponent } from './src/app.component';
+import { AppComponent } from './app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
